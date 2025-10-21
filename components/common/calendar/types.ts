e4bdbb41361297b08ada1fc67da578614572e8a5
Michael Manlulu <michaeldmanlulu@gmail.com>
2025-10-22 01:13:31 +0800

    Add calendar types

---
+++ components/common/calendar/types.ts
@@ -0,0 +1,40 @@
+export type ViewMode = 'day' | 'week' | 'month';
+
+export interface CalendarEventBase {
+  date: Date;
+}
+
+export interface CalendarEventSent extends CalendarEventBase {
+  type: 'sent';
+  loan: any;
+  investors: Array<{
+    name: string;
+    amount: number;
+  }>;
+  totalAmount: number;
+}
+
+export interface CalendarEventDue extends CalendarEventBase {
+  type: 'due';
+  loan: any;
+  totalPrincipal: number;
+  totalInterest: number;
+  totalAmount: number;
+}
+
+export type CalendarEvent = CalendarEventSent | CalendarEventDue;
+
+export interface CalendarCell {
+  date: Date;
+  isCurrentMonth: boolean;
+  events: CalendarEvent[];
+}
+
+export interface CalendarConfig {
+  formatCurrency: (amount: number) => string;
+  onEventClick: (event: CalendarEvent) => void;
+  renderEventCard?: (
+    event: CalendarEvent,
+    eventIndex: number
+  ) => React.ReactNode;
+}
