cb0d719e96c09b12b388e40857737df189070c7d
Michael Manlulu <michaeldmanlulu@gmail.com>
2025-10-22 01:13:01 +0800

    Create reusable calendar component

---
+++ components/common/calendar/calendar.tsx
@@ -0,0 +1,71 @@
+'use client';
+
+import { Card, CardContent } from '@/components/ui/card';
+import { CalendarEvent, CalendarConfig } from './types';
+import { useCalendar } from './use-calendar';
+import { CalendarHeader } from './calendar-header';
+import { CalendarDayView } from './calendar-day-view';
+import { CalendarWeekView } from './calendar-week-view';
+import { CalendarMonthView } from './calendar-month-view';
+
+interface CalendarProps {
+  events: CalendarEvent[];
+  config: CalendarConfig;
+  showLegend?: boolean;
+}
+
+export function Calendar({ events, config, showLegend = true }: CalendarProps) {
+  const {
+    viewMode,
+    setViewMode,
+    calendarData,
+    goToPreviousPeriod,
+    goToNextPeriod,
+    goToToday,
+    getViewTitle,
+    dayNames,
+    isToday,
+  } = useCalendar({ events });
+
+  return (
+    <div className="space-y-4">
+      {/* Calendar Header */}
+      <CalendarHeader
+        title={getViewTitle()}
+        viewMode={viewMode}
+        onViewModeChange={setViewMode}
+        onToday={goToToday}
+        onPrevious={goToPreviousPeriod}
+        onNext={goToNextPeriod}
+        showLegend={showLegend}
+      />
+
+      {/* Calendar Grid */}
+      <Card>
+        <CardContent className="p-0">
+          {viewMode === 'day' && (
+            <CalendarDayView cells={calendarData} config={config} />
+          )}
+
+          {viewMode === 'week' && (
+            <CalendarWeekView
+              cells={calendarData}
+              config={config}
+              dayNames={dayNames}
+              isToday={isToday}
+            />
+          )}
+
+          {viewMode === 'month' && (
+            <CalendarMonthView
+              cells={calendarData}
+              config={config}
+              dayNames={dayNames}
+              isToday={isToday}
+            />
+          )}
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
