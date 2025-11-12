4335a8331d319697af35bf5078cd9885065acf38
Michael Manlulu <michaeldmanlulu@gmail.com>
2025-11-13 00:38:19 +0800

    Improve card component

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      'rounded-lg border bg-card text-card-foreground shadow-none',
+      'rounded-xl border-2 bg-card text-card-foreground shadow-sm transition-all duration-300 hover:shadow-lg',
       className
     )}
     {...props}
