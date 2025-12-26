2bc75f4d38e9ea5ad3661c484d57e8f47951081e
Michael Manlulu <michaeldmanlulu@gmail.com>
2025-12-26 08:25:04 +0800

    Update index.ts

--- components/common/index.ts
+++ components/common/index.ts
@@ -34,6 +34,7 @@ export { Logo } from './logo';
 export { ExportButton } from './export-button';
 export { ExportColumnsModal } from './export-columns-modal';
 export { SyncCalendarButton } from './sync-calendar-button';
+export { NavigationProgressProvider, useNavigationProgress } from './navigation-progress';
 
 // Calendar components
 export * from './calendar';
