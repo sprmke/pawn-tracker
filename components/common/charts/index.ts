a38c84b7f16b147ea029b30efeeaacee2cd537d9
Michael Manlulu <michaeldmanlulu@gmail.com>
2026-01-30 02:45:30 +0800

    Update index.ts

--- components/common/charts/index.ts
+++ components/common/charts/index.ts
@@ -5,3 +5,4 @@ export { CurrencyLineChart } from './currency-line-chart';
 export { CurrencyBarChart } from './currency-bar-chart';
 export { LoanTypePieChart } from './loan-type-pie-chart';
 export { CustomTooltip } from './custom-tooltip';
+export { CashflowTrendChart } from './cashflow-trend-chart';
